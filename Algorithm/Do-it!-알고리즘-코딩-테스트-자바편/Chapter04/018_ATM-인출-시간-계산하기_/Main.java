0f0b417292332b6c7a3fbb5e18570aa9b9b939c9
HeeFlower5001 <gmltmd3tp@naver.com>
2025-03-26 15:05:38 +0900

    2025_03_26

---
+++ Algorithm/Do-it!-알고리즘-코딩-테스트-자바편/Chapter04/018_ATM-인출-시간-계산하기_/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] time = new int[n];
+        int[] sum = new int[n];
+        for (int i = 0; i < n; i++) time[i] = sc.nextInt();
+
+        for (int i1 = 1; i1 < n; i1++) {
+            for (int i2 = i1; i2 < n; i2++) {
+                
+            }
+        }
+
+    }
+}
